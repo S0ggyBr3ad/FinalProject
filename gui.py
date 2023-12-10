d1db86636ec59a3b95033c683e456ed709f8fdd8
Wullff <89040132+Wullff@users.noreply.github.com>
2023-12-10 18:30:01 -0500

    Added buttons for low, mid, and high range graphs

--- gui.py
+++ gui.py
@@ -2,11 +2,14 @@ import os.path
 from tkinter import*
 from tkinter import ttk     # from tkinter import Tk for Python 3.x
 from tkinter.filedialog import askopenfilename
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from convert import *
 from reverb import *
+
+reverb = Reverb()
+
 def selectFile():
     filename = askopenfilename()
-    print(filename) 
     if filename.endswith(".wav"):
         print(filename)
         print("Wav file accepted.")
@@ -19,8 +22,9 @@ def selectFile():
     else:
         print("Please choose a .mp3 or .wav file.")
         return
-    filename_label.config(text = f"Current file: {os.path.basename(filename)}")
-    analyse("temp.wav")
+    filename_label.config(text=f"Current file: {os.path.basename(filename)}")
+    reverb.openFile("temp.wav")
+    print(round(reverb.getTime(), 2), " seconds.")
 
 
 root = Tk()
@@ -30,13 +34,34 @@ root.geometry("300x200")
 _mainframe = ttk.Frame(root, padding='5 5 5 5 ')
 _mainframe.grid(row=0, column=0, sticky=("E", "W", "N", "S"))
 
+canvas = ttk.Canvas(root)
+canvas.pack()
+
 selectBtn = ttk.Button(_mainframe, text='Select File', command=selectFile)
 selectBtn.grid(row=1, column = 0, sticky = W, padx = 10)
+
+lowRT60Btn = ttk.Button(_mainframe, text='Low RT60', command=lambda: reverb.dbplt(250))
+lowRT60Btn.grid(row=3, column = 0, sticky = W, padx = 10)
+
+midRT60Btn = ttk.Button(_mainframe, text='Mid RT60', command=lambda: reverb.dbplt(1000))
+midRT60Btn.grid(row=4, column = 0, sticky = W, padx = 10)
+
+highRT60Btn = ttk.Button(_mainframe, text='High RT60', command=lambda: reverb.dbplt(4000))
+highRT60Btn.grid(row=5, column = 0, sticky = W, padx = 10)
+
+combRT60Btn = ttk.Button(_mainframe, text='Select File', command=selectFile)
+combRT60Btn.grid(row=1, column = 0, sticky = W, padx = 10)
+
+waveformBtn = ttk.Button(_mainframe, text='Select File', command=selectFile)
+waveformBtn.grid(row=1, column = 0, sticky = W, padx = 10)
+
+SpectBtn = ttk.Button(_mainframe, text='Select File', command=selectFile)
+SpectBtn.grid(row=1, column = 0, sticky = W, padx = 10)
 
 fileFrame = ttk.Frame(root, relief='sunken')
 fileFrame.grid(row = 2, column = 0, sticky=("E", "W", "S"))
 fileFrame = ttk.LabelFrame(_mainframe, text = '')
 filename_label = ttk.Label(_mainframe, text ='')
-filename_label.grid(row = 11, column = 0)
+filename_label.grid(row = 2, column = 0)
 
 root.mainloop()
--- reverb.py
+++ reverb.py
@@ -2,85 +2,73 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.io import wavfile
 
-def debugg(fstring):
-    pass
+class Reverb:
+    def __init__(self):
+        self.spectrum = None
+        self.freqs = None
+        self.t = None
+        self.im = None
+        self.data_in_db = None
+        self.range = None
+        self.RT60 = None
 
-# prints var outputs
-def find_target_frequency(freqs):
-    for x in freqs:
-        if x > 1000:
-            break
-        return x
+    def openFile(self, filename):
+        sample_rate, data = wavfile.read(filename)
+        self.spectrum, self.freqs, self.t, self.im = plt.specgram(data, Fs=sample_rate, NFFT=1024, cmap=plt.get_cmap('autumn_r'))
+        plt.clf()
 
-def frequency_check(freqs, spectrum):
+    def getTime(self):
+        return self.t[len(self.t)-1]
 
-    debugg(f'freqs {freqs[:10]}')
-    target_frequency = find_target_frequency(freqs)
-    debugg(f'target_frequency {target_frequency}')
-    index_of_frequency = np.where(freqs == target_frequency)[0][0]
-    debugg(f'index_of_frequency {index_of_frequency}')
+    def frequency_check(self, ranges):
+        index_of_frequency = np.where(self.freqs >= ranges)[0][0]
+        data_for_frequency = self.spectrum[index_of_frequency]
+        # change a digital signal for a values in decibels
+        self.data_in_db = 10 * np.log10(data_for_frequency)
 
-    data_for_frequency = spectrum[index_of_frequency]
-    debugg(f'data_for_frquency {data_for_frequency[:10]}')
+    # find a nearest value
+    def find_nearest_value(self, array, value):
+        array = np.asarray(array)
+        idx = (np.abs(array - value)).argmin()
+        return array[idx]
 
-    # change a digital signal for a values in decibels
+    def dbplt(self, ranges):
+        self.frequency_check(ranges)
+        plt.figure(2)
+        # plot reverb time on grid
+        plt.plot(self.t, self.data_in_db, linewidth=1, alpha=0.7, color='#004bc6')
+        plt.xlabel('Time (s)')
+        plt.ylabel('Power (dB)')
 
-    data_in_db_fun = 10 * np.log10(data_for_frequency)
-    return data_in_db_fun
+        # find an index of a  max value
+        index_of_max = np.argmax(self.data_in_db)
 
+        value_of_max = self.data_in_db[index_of_max]
 
-# find a nearest value
-def find_nearest_value(array, value):
-    array= np.asarray(array)
-    debugg(f'array {array[:10]}')
-    idx = (np.abs(array - value)).argmin()
-    debugg(f'idx {idx}')
-    debugg(f'array[idx] {array[idx]}')
-    return array[idx]
+        plt.plot(self.t[index_of_max], self.data_in_db[index_of_max], 'go')
 
-def analyse(filename):
-    sample_rate, data = wavfile.read(filename)
-    spectrum, freqs, t, im = plt.specgram(data, Fs=sample_rate, NFFT=1024, cmap=plt.get_cmap('autumn_r'))
-    
-    data_in_db = frequency_check(freqs, spectrum)
-    plt.figure()
-    # plot reverb time on grid
-    plt.plot(t, data_in_db, linewidth=1, alpha=0.7, color='#004bc6')
-    plt.xlabel('Time (s)')
-    plt.ylabel('Power (dB)')
+        # slice array from a max value
+        sliced_array = self.data_in_db[index_of_max:]
 
-    # find an index of a  max value
-    index_of_max = np.argmax(data_in_db)
+        value_of_max_less_5 = value_of_max - 5
+        value_of_max_less_5 = self.find_nearest_value(sliced_array, value_of_max_less_5)
 
-    value_of_max = data_in_db[index_of_max]
+        index_of_max_less_5 = np.where(self.data_in_db == value_of_max_less_5)
 
-    plt.plot(t[index_of_max], data_in_db[index_of_max], 'go')
+        plt.plot(self.t[index_of_max_less_5], self.data_in_db[index_of_max_less_5], 'yo')
 
-    # slice array from a max value
-    sliced_array = data_in_db[index_of_max:]
+        # slice array from a max -5dB
+        value_of_max_less_25 = value_of_max - 25
 
-    value_of_max_less_5 = value_of_max - 5
-    value_of_max_less_5 = find_nearest_value(sliced_array, value_of_max_less_5)
+        value_of_max_less_25 = self.find_nearest_value(sliced_array, value_of_max_less_25)
 
-    index_of_max_less_5 = np.where(data_in_db == value_of_max_less_5)
+        index_of_max_less_25 = np.where(self.data_in_db == value_of_max_less_25)
 
-    plt.plot(t[index_of_max_less_5], data_in_db[index_of_max_less_5], 'yo')
+        plt.plot(self.t[index_of_max_less_25], self.data_in_db[index_of_max_less_25], 'ro')
+        rt20 = (self.t[index_of_max_less_5] - self.t[index_of_max_less_25])[0]
 
-    # slice array from a max -5dB
-    value_of_max_less_25 = value_of_max - 25
+        # extrapolate rt20 to rt60
+        self.RT60 = 3*rt20
 
-    value_of_max_less_25 = find_nearest_value(sliced_array, value_of_max_less_25)
-
-    index_of_max_less_25 = np.where(data_in_db == value_of_max_less_25)
-
-    plt.plot(t[index_of_max_less_25], data_in_db[index_of_max_less_25], 'ro')
-    rt20 = (t[index_of_max_less_5] - t[index_of_max_less_25])[0]
-
-    # extrapolate rt20 to rt60
-    rt60 = 3*rt20
-
-    # optional set limits on plot
-    plt.grid()
-    plt.show()
-
-    print(f'The RT60 reverb time is {round(abs(rt60), 2)} seconds')
+        # optional set limits on plot
+        return plt
