0757807cf13047dc1d773025dadc94d799835ef4
Wullff <89040132+Wullff@users.noreply.github.com>
2023-12-11 00:19:30 -0500

    Complete overhaul of gui and reverb, changes to conver
    gui.py
    - Added 6 buttons for displaying different graphs
    - Added text for displaying information about the sound file
    - Added comments
    
    convert.py
    - Removed some unnecessary lines
    - Added comments
    
    reverb.py 
    - Rewrote all functions as methods of a "Reverb" class
    - Added functions for each type of graph
    - Added comments

--- convert.py
+++ convert.py
@@ -1,23 +1,13 @@
-from os import path
-import pydub
 from pydub import AudioSegment
-from pydub.playback import play
-#files
-src = "16bit1chan.mp3"
-dst = "clap.wav"
 
-#convert mp3 to wav
-def convertToWav(filename):
-    pydub.AudioSegment.ffmpeg = filename
-    sound = AudioSegment.from_mp3(src)
-    sound.export(dst, format="wav")
+# Converts mp3 to wav
+def mp3towav(filename):
+    # convert mp3 to wav
+    sound = AudioSegment.from_mp3(filename)
+    sound.export("pt_mono.wav", format="wav")
 
-def convert1Chan(filename):
+# Converts multichannel to mono, also has the side effect of cleaning metadata
+def toMono(filename):
     raw_audio = AudioSegment.from_file(filename, format="wav")
-    channel_count = raw_audio.channels
-    print(channel_count)
     mono_wav = raw_audio.set_channels(1)
     mono_wav.export("pt_mono.wav", format="wav")
-    mono_wav_audio = AudioSegment.from_file("pt_mono.wav", format="wav")
-    channel_count = mono_wav_audio.channels
-    print(channel_count)
